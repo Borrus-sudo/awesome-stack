88cb7c97c5c72cde3c882eb3002bb718e0b71469
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-08-02 09:48:51 +0530

    feat: getting all files from the repo

--- src/utils/fetchInfo.js
+++ src/utils/fetchInfo.js
@@ -1,30 +1,39 @@
 const fetch = require("node-fetch");
 const path = require("path");
-const getRepos = async(name) => {
-    const repos = [];
-    const data = await fetch(`https://api.github.com/users/${name}/repos`).then(
-        (res) => res.json()
-    );
-    data.forEach((repo) => {
-        repos.push(repo.full_name);
-    });
-    return repos;
-};
+// https://raw.githubusercontent.com/${metadata.name}/${repo}/${default_branch}
 module.exports = async function(metadata) {
-    const url = `https://api.github.com/repos/${metadata.name}/${metadata.repo}`;
-    const files = [];
-    const repos = await getRepos(metadata.name);
-    const { default_branch, git_commits_url, html_url } = await fetch(url).then(
-        (res) => res.json()
-    );
-    const payload_url = git_commits_url.replace(
-        "commits{/sha}",
-        `trees/${default_branch}?recursive=1`
-    );
-    console.log(html_url);
-    const payload_data = await fetch(payload_url).then((res) => res.json());
-    for (let file of payload_data.tree) {
-        files.push(path.parse(file.path).base);
+    const baseUrl = `https://api.github.com/repos`;
+    const repoFile = new Map();
+    if (!(metadata.name && metadata.repos)) {
+        return {
+            message: "404 name and repos parameter is mandatory",
+        };
     }
-    return { files, repos };
+    for (const repo of metadata.repos.split(",").slice(0, 5)) {
+        const data = await fetch(baseUrl + `/${metadata.name}/${repo}`).then(
+            (res) => res.json()
+        );
+        const files = [];
+        if (data.message === "Not Found") {
+            return {
+                message: `The repo '${repo}' or username '${metadata.name}' does not seem to exist`,
+            };
+        }
+        const { git_commits_url, default_branch } = data;
+        const payload_url = (git_commits_url || "").replace(
+            "commits{/sha}",
+            `trees/${default_branch}?recursive=1`
+        );
+        if (payload_url) {
+            const payload_data = await fetch(payload_url).then((res) => res.json());
+            if (Array.isArray(payload_data.tree))
+                for (let file of payload_data.tree) {
+                    files.push(file.path);
+                }
+        }
+        const repoUrl = `https://raw.githubusercontent.com/${metadata.name}/${repo}/${default_branch}`;
+        repoFile.set(repoUrl, files);
+    }
+    console.log(repoFile);
+    return { files: repoFile.entries() };
 };
