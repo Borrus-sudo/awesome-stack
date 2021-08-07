f1404b6c42af6b853a098f72cf801c5079781bd5
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-08-07 22:41:33 +0530

    style: better variable name

--- src/api/card.js
+++ src/api/card.js
@@ -4,9 +4,9 @@ const fetchInfo = require("../utils/fetchInfo");
 const createCard = require("../utils/createCard");
 router.get("/", async(req, res) => {
     const query = req.query;
-    const payload = await fetchInfo(query);
+    const ctx = await fetchInfo(query);
     const theme = query.theme || "default";
-    const svgFile = createCard(payload, theme);
+    const svgFile = createCard(ctx, theme);
     res.send(svgFile);
 });
 
