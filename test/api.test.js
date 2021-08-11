123f7014517317dce70448d1a1f86520c59a741f
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-08-11 09:01:42 +0530

    style: restyle unit tests

--- test/api.test.js
+++ test/api.test.js
@@ -38,24 +38,25 @@ describe("GET /api/v1/cards/json", () => {
                 },
                 done
             );
+
+    });
+    it("responds with a json message again", (done) => {
         request(app)
             .get("/api/v1/cards/json?name=Borrus-sudo&repos=vue-generator-graph")
             .set("Accept", "application/json")
             .expect("Content-Type", /json/)
-            .expect(
-                200, {
-                    html: ["html", "html"],
-                    css: ["css"],
-                    javascript: ["javascript", "typescript"],
-                    "testing-frameworks": ["mocha"],
-                    "seo-addons": [],
-                    "package-managers": ["yarn"],
-                    nodejs: ["nodejs"],
-                    platforms: [],
-                    bundlers: ["webpack"],
-                    tools: ["eslint"],
-                },
-            );
+            .expect(200, {
+                html: ["html", "html"],
+                css: ["css"],
+                javascript: ["javascript", "typescript"],
+                "testing-frameworks": ["mocha"],
+                "seo-addons": [],
+                "package-managers": ["yarn"],
+                nodejs: ["nodejs"],
+                platforms: [],
+                bundlers: ["webpack"],
+                tools: ["eslint"],
+            }, done);
     });
     it("responds with not found message", (done) => {
         const username = "JSY";
