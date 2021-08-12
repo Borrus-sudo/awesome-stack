4501eb7a17f6c45f103aff860ab1a244ea64ca0c
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-08-12 23:19:43 +0530

    fix: rolling back a few things

--- src/utils/fetchInfo.js
+++ src/utils/fetchInfo.js
@@ -54,7 +54,7 @@ const svgIdentifier = (dependencies, ecosystem, contents) => {
         });
     }
     //Doing hacky stuff
-    if (!localEcosystem["bundlers"].includes("webpack") &&
+    if (localEcosystem["bundlers"].length === 0 &&
         (localEcosystem["seo-addons"].includes("nuxt") ||
             localEcosystem["seo-addons"].includes("next") ||
             localEcosystem["front-end-frameworks"].includes("vue") ||
